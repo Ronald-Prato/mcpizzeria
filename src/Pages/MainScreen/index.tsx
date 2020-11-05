66afb5bc06c6291d9f0bf0b9f12b263353ff441f
root <ronald.prato@unet.edu.ve>
2020-11-04 23:17:54 -0500

    session logic complete

--- src/Pages/MainScreen/index.tsx
+++ src/Pages/MainScreen/index.tsx
@@ -1,50 +1,106 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MainWrapper, BackgroundImage, BackgroundImageContainer, MainContentLogin, ButtonMessage, LogoImg, Button, ForgotPasswordMessage, BigMessage, SmallMessage, Icon, InputContainer, Input } from './styles';
+
+import Swal from 'sweetalert2'
 
 import logo from '../../assets/logo.png';
 import backgroundPizza from '../../assets/Pizza.png';
 import personIcon from '../../assets/ic_usuario.png';
 import lockIcon from '../../assets/ic_password.png';
 
-import { URL } from '../../constants';
+import API from '../../api.json';
+
+import { Toast } from '../../alerts';
 
 const COMP_NAME = 'main-screen';
 
 const MainScreen = () => {
-  
-  const getInitialJson = () => {
-    fetch(URL)
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+  const users = API.response.users;
+  const [credentials, setCredentials] = useState({username: '', password: ''});
+  const [alertMessage, setAlertMessage] = useState('');
+  const [hasLogin, setHasLogin] = useState(false);
+
+  const checkCurrentSession = () => {
+    if (localStorage.getItem('loggedUser')) {
+      setHasLogin(true);
+    }
   };
-  useEffect(getInitialJson, []);
+  useEffect(checkCurrentSession, []);
+
+  const fireAlertMessage = () => {
+    alertMessage.length &&
+    Toast.fire({
+      icon: 'error',
+      title: alertMessage
+    })
+  };
+  useEffect(fireAlertMessage, [alertMessage]);
+
+  const validators = () => {
+    const registeredUserNames = users.map((user) => user.email);
+    const currentUserObj = users.filter((user) => user.email === credentials.username)[0];
+
+    if (credentials.username === '' || credentials.password.trim() === '') {
+      setAlertMessage('No pueden quedar espacios en blanco');
+      return false; 
+    } else if (!registeredUserNames.includes(credentials.username)) { //if typed user is not in the json
+      setAlertMessage('El usuario no existe');
+      return false; 
+    } else if (credentials.password !== currentUserObj.password) { //if typed user password isn't correct
+      setAlertMessage('Contraseña incorrecta');
+      return false;    
+    }
+    return currentUserObj;
+  }
+  
+  const handleLogin = () => {
+    const isUser = validators();
+    if (isUser) {
+      setHasLogin(true);
+      Toast.fire({
+        icon: 'success',
+        title: `Bienvenido ${isUser.name}`
+      })
+      localStorage.setItem('loggedUser', JSON.stringify(isUser));
+    }
+  };
 
   return (
     <MainWrapper className={`${COMP_NAME}__main-wrapper`}>
       <BackgroundImageContainer>
         <BackgroundImage src={backgroundPizza}/>
       </BackgroundImageContainer>
-      <MainContentLogin>
-        <LogoImg src={logo}/>
-        <BigMessage> Bienvenido </BigMessage>
-        <SmallMessage> A las mejores pizzas del país </SmallMessage>
+      {
+        !hasLogin ?
+        <MainContentLogin>
+          <LogoImg src={logo}/>
+          <BigMessage> Bienvenido </BigMessage>
+          <SmallMessage> A las mejores pizzas del país </SmallMessage>
 
-        <InputContainer className={`${COMP_NAME}__main-wrapper_main-content_input-container_username`}>
-          <Input placeholder={'Usuario'}/>
-          <Icon src={personIcon}/> 
-        </InputContainer>
+          <InputContainer className={`${COMP_NAME}__main-wrapper_main-content_input-container_username`}>
+            <Input onChange={(event) => setCredentials({...credentials, username: event.target.value.trim()})} value={credentials.username} placeholder={'Usuario (email)'}/>
+            <Icon src={personIcon}/> 
+          </InputContainer>
 
-        <InputContainer className={`${COMP_NAME}__main-wrapper_main-content_input-container_password`}>
-          <Input type={'password'} placeholder={'Contraseña'}/>
-          <Icon src={lockIcon}/> 
-        </InputContainer>
+          <InputContainer className={`${COMP_NAME}__main-wrapper_main-content_input-container_password`}>
+            <Input onChange={(event) => setCredentials({...credentials, password: event.target.value})} value={credentials.password} type={'password'} placeholder={'Contraseña'}/>
+            <Icon src={lockIcon}/> 
+          </InputContainer>
 
-        <ForgotPasswordMessage> ¿Olvidaste tu contraseña? </ForgotPasswordMessage>
+          <ForgotPasswordMessage onClick={() => {
+            Toast.fire({
+              icon: 'error',
+              title: 'Me encantaría comprobar tu cuenta, enviarte un código de verificación y cambiar tu contraseña pero la base de datos en un JSON :\'v'
+            })
+          }}> ¿Olvidaste tu contraseña? </ForgotPasswordMessage>
 
-        <Button>
-          <ButtonMessage> Iniciar Sesión </ButtonMessage>
-        </Button>
-      </MainContentLogin>
+          <Button onClick={handleLogin}>
+            <ButtonMessage> Iniciar Sesión </ButtonMessage>
+          </Button>
+        </MainContentLogin>
+        :
+        <div onClick={() => localStorage.removeItem('loggedUser')}> aasdasdasd </div>
+      }
     </MainWrapper>
   );
 };
