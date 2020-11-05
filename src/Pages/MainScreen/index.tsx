15b124de651d03be68057f6465a5c8dda6604463
root <ronald.prato@unet.edu.ve>
2020-11-04 22:22:38 -0500

    login view finished

--- src/Pages/MainScreen/index.tsx
+++ src/Pages/MainScreen/index.tsx
@@ -17,7 +17,6 @@ const MainScreen = () => {
       .then(res => console.log(res))
   };
   useEffect(getInitialJson, []);
-  
 
   return (
     <MainWrapper className={`${COMP_NAME}__main-wrapper`}>
